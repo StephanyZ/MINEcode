cbdd463342904f945e472ccf51661c9b6ece7ac9
StephanyZ <923036400@qq.com>
2017-03-30 14:13:52 +0800

    share_memory_1

---
+++ share_memory/shmdata.h
@@ -0,0 +1,20 @@
+/*===============================================================================
+    > File Name: shmdata.h
+    > Author: xfzhang
+    > Mail: 923036400@qq.com 
+    > Created Time: 2017年03月30日 星期四 09时51分30秒
+ =======================================================================**/
+#ifndef _SHMDATA_H_HEADER
+#define _SHMDATA_H_HEADER
+
+#define TEXT_SZ 2048
+
+struct shared_use_st
+{
+	int written;//作为一个标志，非0：表示可读，0表示可写
+	char text[TEXT_SZ];//记录写入和读取的文本
+};
+
+#endif
+
+
